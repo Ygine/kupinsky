79b9cd21454a0cf7edf97858ecb031ec724485fc
vitvitskyi <vitvitskyi.vd@gmail.com>
2024-04-11 12:21:26 +0200

    add Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import Test from "./components/Test"
+import Header from "./components/Header"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="test">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header />
+      <div className="App">
+        <header className="App-header">
+          <p className="test">
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
 
-      <Test />
-    </div>
+        <Test />
+      </div>
+      </>
   );
 }
 
